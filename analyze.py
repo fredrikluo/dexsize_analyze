04cad5f09b7f9034f1d03aaaa35aa87d3ac89b69
Fredrik Zhiyu Luo <zhiyul@opera.com>
2015-04-23 12:25:05 +0800

    initial checkin.

--- analyze.py
+++ analyze.py
@@ -27,69 +27,90 @@ def reset_methodEntry(m):
 	m['name'] = ""
 	m['size'] = 0
 
-# Data containers
-class_list = []
-class_entry = {}
-method_entry = {}
+def add_class(class_entry, class_list):
+	# Push the old class into list
+	class_list.append(dict(class_entry))
+	reset_classEntry(class_entry)
 
-# State machine
-NOP = 0
-CLASS = 1
-METHOD = 2
-state = NOP
+def add_method_to_class(class_entry, method_entry):
+	class_entry['method_list'].append(method_entry['name'] + ":"+str(method_entry['size']))
+	class_entry['size'] += method_entry['size']
+	reset_methodEntry(method_entry)
 
-# Parsing the dex dump file
-dex_file = open(sys.argv[1]).readlines()
+def analyzeDex(class_list, dex_file):
+	# Data containers
+	class_entry = {}
+	method_entry = {}
+	
+	# State machine
+	NOP = 0
+	CLASS = 1
+	METHOD = 2
+	state = NOP
 
-reset_classEntry(class_entry)
-reset_methodEntry(method_entry)
+	reset_classEntry(class_entry)
+	reset_methodEntry(method_entry)
 
-for l in dex_file:
-	l = l.strip() 
+	for l in dex_file:
+		l = l.strip() 
 
-	if state ==  NOP:
-		if isClassStart(l):
-			state = CLASS
+		if state ==  NOP:
+			if isClassStart(l):
+				state = CLASS
 
-	elif state ==  CLASS:
-		# New class starts
-		# Push the old class into list
-		if class_entry['class_des'] != "":
-			class_list.append(dict(class_entry))
-			reset_classEntry(class_entry)
+		elif state ==  CLASS:
+			# New class starts
+			if class_entry['class_des'] != "" and class_entry['size'] > 0:
+				add_class(class_entry, class_list)
 
-		if isClassDescriptor(l): 
-			class_entry['class_des']= getContent(l)
+			if isClassDescriptor(l): 
+				class_entry['class_des']= getContent(l)
 
-		if isMethodStart("#"):
-			state = METHOD
+			if isMethodStart(l):
+				state = METHOD
 
-	elif state == METHOD:
-		if isClassStart(l):
-			state = CLASS
+		elif state == METHOD:
+			new_class =  isClassStart(l)
 
-		if isMethodStart(l):
-			#new method, push the last method entry into class:
-			if method_entry['size'] > 0:
-				class_entry['method_list'].append(method_entry['name'] + ":"+str(method_entry['size']))
-				class_entry['size'] += method_entry['size']
+			if isMethodStart(l) or new_class:
+				#new method, push the last method entry into class:
+				if method_entry['size'] > 0:
+					add_method_to_class(class_entry, method_entry)
+			elif isMethodName(l):
+				method_entry['name'] = getContent(l)
+			elif isMethodSize(l):
+				c, bit = l.split(":")[1].strip().split("-bit")[0].split()
+				foot_print = int(c) * int(bit) / 8
+				method_entry['size'] = foot_print
 
-			reset_methodEntry(method_entry)
+			if new_class:
+				state = CLASS
+		else:
+			print "can't happen"
 
-		if isMethodName(l):
-			method_entry['name'] = getContent(l)
+	# Push the last item into list 
+	if method_entry['size'] > 0:
+		add_method_to_class(class_entry, method_entry)
 
-		if isMethodSize(l):
-			c, bit = l.split(":")[1].strip().split("-bit")[0].split()
-			foot_print = int(c)*int(bit)/8
-			method_entry['size'] = foot_print
-	else:
-		print "can't happen"
+	if class_entry['class_des'] != "" and class_entry['size'] > 0:
+		add_class(class_entry, class_list)
+	
+def _main():
+	# Parsing the dex dump file
+	dex_file = open(sys.argv[1]).readlines()
+
+	class_list = []
+	analyzeDex(class_list, dex_file)
+	class_list = sorted(class_list, key = lambda i:i['size'])
+
+	sum_size = 0
+	for i in class_list:
+		sum_size += i['size'] + 4
+		print i['class_des'][2:-2].replace('/', '.')+","+str(i['size'])
+#		for c in i['method_list']:
+#			print c
+
+	print "Summary number of class {0}, size {1}".format(len(class_list), sum_size)
 
 
-class_list = sorted(class_list, key = lambda i:i['size'])
-
-for i in class_list:
-	print "Class descriptor:"+i['class_des']+" size:"+str(i['size'])
-	for c in i['method_list']:
-		print "   "+ c
+_main()
